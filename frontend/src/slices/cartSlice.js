ebfccb39b548d5ab1f1bcfcd2de80a29ecfce856
Nerakma84 <fnk8476@gmail.com>
2024-01-14 01:09:26 -0600

    placeOrderScreen completed with checkout steps

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -37,10 +37,19 @@ addToCart: (state, action) => {
     savePaymentMethod: (state, action) => {
         state.paymentMethod = action.payload;
         return updateCart(state);
-    }
+    },
+    clearCartItems: (state, action) => {
+        state.cartItems = [];
+        return updateCart(state);
+    },
 },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod } = cartSlice.actions;
+export const { 
+    addToCart, 
+    removeFromCart, 
+    saveShippingAddress, 
+    savePaymentMethod, 
+    clearCartItems } = cartSlice.actions;
 
 export default cartSlice.reducer;
